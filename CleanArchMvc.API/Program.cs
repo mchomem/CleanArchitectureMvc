45be3f0e860860f9578f2dc804fd8f50a0bf7c21
Misael C. Homem <misael.homem@gmail.com>
2024-04-28 02:06:56 -0300

    fix(API): .net core framework update from 5.0 to 8.0

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -1,15 +1,59 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Hosting;
+using CleanArchMvc.Infra.IoC;
+using Microsoft.OpenApi.Models;
+using System.Reflection;
 
-namespace CleanArchMvc.API
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddInfrastructureAPI(builder.Configuration);
+//ativar autenticacao e validar o token
+builder.Services.AddInfrastructureJWT(builder.Configuration);
+builder.Services.AddInfrastructureSwagger();
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
 {
-    public class Program
+    options.SwaggerDoc("v2", new OpenApiInfo
     {
-        public static void Main(string[] args)
-            => CreateHostBuilder(args).Build().Run();
+        Version = "v2",
+        Title = "CleanArchMvc API",
+        Description = "Projeto API",
+        TermsOfService = new Uri("https://examplp.com/termoservico"),
+        Contact = new OpenApiContact
+        {
+            Name = "Contato",
+            Url = new Uri("https://examplo.com/contato")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "Licença",
+            Url = new Uri("https://examplo.com/licenca")
+        }
+    });
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder => { webBuilder.UseStartup<Startup>(); });
-    }
+    // usando System.Reflection;
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
+
+app.UseHttpsRedirection();
+
+app.UseStatusCodePages();
+app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
